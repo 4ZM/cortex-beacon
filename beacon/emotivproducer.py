b984756f181b0a43549ddd68d9408494c8c59783
Anders Sundman <anders@4zm.org>
2013-09-28 13:21:24 +0200

    Producer of emotiv data
    Note: Currently contains buffering bug that makes data rate uneven

--- beacon/emotivproducer.py
+++ beacon/emotivproducer.py
@@ -1,12 +1,28 @@
 from twisted.internet import protocol, reactor, endpoints, interfaces
+from emokit import emotiv
+import gevent
+import time
+
+sensor_names = { 'X', 'Y',
+                 'F3', 'FC5', 'AF3', 'F7', 'T7', 'P7',
+                 'O1', 'O2', 'P8', 'T8', 'F8', 'AF4',
+                 'FC6', 'F4', 'Unknown' }
 
 class EmotivProducer:
+
     """Sends an infinit stream of emotiv data in the the cortext-beacon format"""
+
     __implements__ = interfaces.IPushProducer
 
     def __init__(self, proto):
         self._proto = proto
         self._paused = False
+
+        self._headset = emotiv.Emotiv()
+
+        # Start the emotiv data grabbing in a separate thread
+        gevent.spawn(self._headset.setup)
+        gevent.sleep(1)
 
     def pauseProducing(self):
         self._paused = True
@@ -14,7 +30,16 @@ class EmotivProducer:
     def resumeProducing(self):
         self._paused = False
         while not self._paused:
-            self._proto.transport.write("whee e ")
+            try:
+                packet = self._headset.dequeue()
+            except KeyboardInterrupt:
+                print "Disconnected client. Ctrl-C again to stop the server."
+                return
+            sensors = packet.sensors
+            data_str = ",".join(str(sensors[x]['value']) for x in sensor_names)
+            self._proto.transport.write(data_str + '\n')
 
     def stopProducing(self):
-        pass
+        self._headset.close()
+        self._headset = None
+        gevent.shutdown()
